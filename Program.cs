d2648320496d874e0e3e84d31e8aad08079376de
rsw114 <richard.walker3@student.anglia.ac.uk>
2015-05-30 19:55:58 +0100

    3s and 5s
    This program takes the multiples of 3 and 5 under 1000 adds them
    together and prints the answer.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiples_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0; //set total counter
+
+            for (int i = 1; i < 1000; i++) 
+            {
+                if ((i % 3 == 0) || (i % 5 == 0)) // test if i divides by 3 or 5 with no remainder
+                {
+                    total = total + i; // add to total
+                }
+            }
+            Console.WriteLine(total); //get answer
+            Console.ReadLine();
+        }
+    }
+}
