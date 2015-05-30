377a2133105edbe01e16599b0bf72560c00472c1
rsw114 <richard.walker3@student.anglia.ac.uk>
2015-05-30 20:21:31 +0100

    Even fibonacci
    This takes the fibonacci numbers under 4 million and adds the even
    numbers together and prints the sum total of the even numbers at the
    end.

---
+++ Even Fibonacci/Even Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Even_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int total = 0;
+
+            do
+            {
+                a = a + b; // add the numbers in sequence
+                if (a % 2 == 0) // check even number
+                {
+                    total = total + a; // add to sum
+                }
+                Console.Write(a + " ");
+
+                b = a + b; // same but for b
+                if (b % 2 == 0)
+                {
+                    total = total + b;
+                }
+                Console.Write(b + " ");
+            }
+            while (total < 4000000);
+            Console.WriteLine("\n" +total);
+            Console.ReadLine();
+        }
+    }
+}
